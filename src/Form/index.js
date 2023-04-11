64055a523d7e9fe861b131496b83ee02148514d7
a-maczynska <agaa.mac@gmail.com>
2023-04-11 18:55:34 +0200

    adding new task to the list is working

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,13 +1,25 @@
 import "./style.css";
+import { useState } from "react";
 
-const Form = () => (
-  <form className="form">
-    <input
-      type="text"
-      className="form__textInput"
-      placeholder="Co jest do zrobienia?"
-    />
-    <button className="form__button">Dodaj zadanie</button>
-  </form>
-);
+const Form = ({ addNewTask }) => {
+  const [newTaskContent, setNewTaskContent] = useState("");
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    addNewTask(newTaskContent.trim());
+  };
+
+  return (
+    <form className="form" onSubmit={onFormSubmit}>
+      <input
+        value={newTaskContent}
+        type="text"
+        className="form__textInput"
+        placeholder="Co jest do zrobienia?"
+        onChange={(event) => setNewTaskContent(event.target.value)}
+      />
+      <button className="form__button">Dodaj zadanie</button>
+    </form>
+  );
+};
 export default Form;
